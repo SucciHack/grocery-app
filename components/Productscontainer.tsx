f3afc3676c1583074f91106004c1ac1a92fb741f
Kaweesi Samuel <158592803+SucciHack@users.noreply.github.com>
2025-01-14 19:25:55 +0300

    update

--- components/Productscontainer.tsx
+++ components/Productscontainer.tsx
@@ -6,7 +6,7 @@ import { fetchProducts } from '@/Action/Products'
 export default async function Productscontainer() {
     const products = await fetchProducts()
   return (
-    <div className='bg-white w-[98%] mx-auto rounded-lg overflow-hidden flex gap-2 h-[600px]'>
+    <div className='bg-white w-[98%] mx-auto rounded-lg overflow-hidden flex gap-2 h-[600px] mb-5'>
       <div><Sidebar/></div>
       <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 p-2 overflow-scroll scrollbar-hide gap-2 w-[80%] items-end'>
         {
