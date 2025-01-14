5365e58877d7ec6a8a55aa9396f74826721f7311
Kaweesi Samuel <158592803+SucciHack@users.noreply.github.com>
2025-01-14 19:19:19 +0300

    updated container

--- components/Productscontainer.tsx
+++ components/Productscontainer.tsx
@@ -8,7 +8,7 @@ export default async function Productscontainer() {
   return (
     <div className='bg-white w-[98%] mx-auto rounded-lg overflow-hidden flex gap-2 h-[600px]'>
       <div><Sidebar/></div>
-      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 p-2 overflow-scroll scrollbar-hide gap-2 w-[80%] items-end'>
+      <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 p-2 overflow-scroll scrollbar-hide gap-2 w-[80%] items-end'>
         {
           products?.map((product)=>{
             return <Productcard key={product.id} product={product}/>
